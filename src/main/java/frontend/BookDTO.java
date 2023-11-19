8b06483e7dd5befe1308ffb23fb6f69329624630
yaqout salameh <yaqoutsalameh@gmail.com>
2023-11-19 20:56:34 +0200

    Adding the Book and BookDTO classes

---
+++ src/main/java/frontend/BookDTO.java
@@ -0,0 +1,29 @@
+package frontend;
+
+public class BookDTO {
+	private int id;
+	private String title;
+	
+	public BookDTO(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+	public BookDTO(Book book) {
+		this.id = book.getId();
+		this.title = book.getTitle();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
+
